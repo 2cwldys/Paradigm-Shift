1a4bfb3ea606bc8540048c7b2ca2d5d93d583837
Steel <22steelknight22@gmail.com>
2019-06-02 05:28:30 -0400

    Added in a weapon. 'SapphireLightRifle'

---
+++ IN PROGRESS/Ashton.StEEL/SapphireLightRifle/plugin/items/sh_cw_sapphireLR.lua
@@ -0,0 +1,46 @@
+-----------------------------------------------------
+--[[
+
+    © 2013 CloudSixteen.com do not share, re-distribute or modify
+
+    without permission of its author (kurozael@gmail.com).
+
+--]]
+
+
+
+local ITEM = Clockwork.item:New("weapon_base");
+
+    ITEM.name = "Sapphire Light Rifle";
+
+    ITEM.cost = 20000;
+
+    ITEM.model = "models/weapons/w_irifle.mdl";
+
+    ITEM.weight = 10;
+
+    ITEM.access = "V";
+
+    ITEM.type = "Main";
+
+    ITEM.uniqueID = "sfw_saphyre";
+
+    ITEM.business = true;
+
+    ITEM.description = "A light rifle with a burst fire mechanism, Commonly used by the 'Enforcers'.";
+
+    ITEM.isAttachment = true;
+
+    ITEM.hasFlashlight = true;
+
+    ITEM.loweredOrigin = Vector(3, 0, -4);
+
+    ITEM.loweredAngles = Angle(0, 45, 0);
+
+    ITEM.attachmentBone = "ValveBiped.Bip01_Spine";
+
+    ITEM.attachmentOffsetAngles = Angle(0, 0, 0);
+
+    ITEM.attachmentOffsetVector = Vector(-3.96, 4.95, -2.97);
+
+ITEM:Register();
---
+++ IN PROGRESS/Ashton.StEEL/SapphireLightRifle/plugin/sv_hooks.lua
@@ -0,0 +1,6 @@
+local PLUGIN = PLUGIN;
+local Schema = Schema;
+
+function Schema:PlayerGiveWeapons(ply)
+	-- No weps needed. At all.
+end;
