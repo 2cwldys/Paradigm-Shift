b0bcd562252bdcb622cedd7caa360d59a8fe6d31
Stephen Payne <sdpayne113@gmail.com>
2019-06-02 02:01:45 -0400

    added lapis pulsepistol from darken217's scifi weapons

---
+++ IN PROGRESS/2cwldys/pulsepistol/plugin/items/sh_cw_pulsepistol.lua
@@ -0,0 +1,46 @@
+-----------------------------------------------------
+--[[
+
+    © 2013 CloudSixteen.com do not share, re-distribute or modify
+
+    without permission of its author (kurozael@gmail.com).
+
+--]]
+
+
+
+local ITEM = Clockwork.item:New("weapon_base");
+
+    ITEM.name = "Lapis Pulse Pistol";
+
+    ITEM.cost = 11000;
+
+    ITEM.model = "models/Items/battery.mdl";
+
+    ITEM.weight = 8;
+
+    ITEM.access = "V";
+
+    ITEM.type = "Main";
+
+    ITEM.uniqueID = "sfw_lapis";
+
+    ITEM.business = true;
+
+    ITEM.description = "A packaged Lapis pulse pistol, commonly manufactured and bought for self protection uses.";
+
+    ITEM.isAttachment = true;
+
+    ITEM.hasFlashlight = true;
+
+    ITEM.loweredOrigin = Vector(3, 0, -4);
+
+    ITEM.loweredAngles = Angle(0, 5, 0);
+
+    ITEM.attachmentBone = "ValveBiped.Bip01_Spine";
+
+    ITEM.attachmentOffsetAngles = Angle(0, -10, -175);
+
+    ITEM.attachmentOffsetVector = Vector(-2, 5, 10);
+
+ITEM:Register();
