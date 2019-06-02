a9255912b061be5219678c24d24a9e97023526a9
Stephen Payne <sdpayne113@gmail.com>
2019-06-02 06:51:07 -0400

    weapon updates

--- DEVSPACE/2cwldys/vaporcannon/plugin/items/sh_cw_vaporcannon.lua
+++ DEVSPACE/2cwldys/vaporcannon/plugin/items/sh_cw_vaporcannon.lua
@@ -11,23 +11,23 @@
 
 local ITEM = Clockwork.item:New("weapon_base");
 
-    ITEM.name = "Sapphire Light Rifle";
+    ITEM.name = "Vapor Cannon";
 
-    ITEM.cost = 20000;
+    ITEM.cost = 40000;
 
     ITEM.model = "models/weapons/w_irifle.mdl";
 
-    ITEM.weight = 10;
+    ITEM.weight = 8;
 
     ITEM.access = "V";
 
     ITEM.type = "Main";
 
-    ITEM.uniqueID = "sfw_saphyre";
+    ITEM.uniqueID = "sfw_vapor";
 
     ITEM.business = true;
 
-    ITEM.description = "A light rifle with a burst fire mechanism, commonly used by the Enforcer bots. Manufactured by the Guinan Corporation.";
+    ITEM.description = "A vapor cannon, utilizing humidity in the air to create particle energy, used commonly by high-threat protocol SHEPHERD bots. Manufactured by the Guinan Corporation.";
 
     ITEM.isAttachment = true;
 
@@ -35,12 +35,12 @@ local ITEM = Clockwork.item:New("weapon_base");
 
     ITEM.loweredOrigin = Vector(3, 0, -4);
 
-    ITEM.loweredAngles = Angle(0, 45, 0);
+    ITEM.loweredAngles = Angle(0, 5, 0);
 
     ITEM.attachmentBone = "ValveBiped.Bip01_Spine";
 
-    ITEM.attachmentOffsetAngles = Angle(0, 0, 0);
+    ITEM.attachmentOffsetAngles = Angle(0, -10, -175);
 
-    ITEM.attachmentOffsetVector = Vector(-3.96, 4.95, -2.97);
+    ITEM.attachmentOffsetVector = Vector(-2, 5, 10);
 
 ITEM:Register();
